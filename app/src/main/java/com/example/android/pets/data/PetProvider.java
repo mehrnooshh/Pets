a41182c3cb25c00ba21e5733cce6be99293dbd24
Mehrnoosh Hassani <hassani.mehrnoosh@gmail.com>
2018-02-21 12:16:00 -0800

    Implemented update() method in ContentProvider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,9 +1,5 @@
 package com.example.android.pets.data;
 
-/**
- * Created by Mehrnoosh on 2/17/2018.
- */
-
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -11,12 +7,16 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import static com.example.android.pets.data.PetContract.CONTENT_AUTHORITY;
 import static com.example.android.pets.data.PetContract.PetEntry.TABLE_NAME;
 
+/**
+ * Created by Mehrnoosh on 2/17/2018.
+ */
 public class PetProvider extends ContentProvider {
 
     /** Tag for the log messages */
@@ -62,7 +62,7 @@ public class PetProvider extends ContentProvider {
      * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
      */
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
         // Get readable database
         SQLiteDatabase database = mDbHelper.getReadableDatabase();
@@ -106,7 +106,7 @@ public class PetProvider extends ContentProvider {
      * Insert new data into the provider with the given ContentValues.
      */
     @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
+    public Uri insert(@NonNull Uri uri, ContentValues contentValues) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case PETS:
@@ -162,16 +162,78 @@ public class PetProvider extends ContentProvider {
          */
 
         @Override
-        public int update (Uri uri, ContentValues contentValues, String selection, String[]
-        selectionArgs){
+        public int update(@NonNull Uri uri, ContentValues contentValues, String selection,
+                          String[] selectionArgs) {
+            final int match = sUriMatcher.match(uri);
+            switch (match) {
+                case PETS:
+                    return updatePet(uri, contentValues, selection, selectionArgs);
+                case PET_ID:
+                    // For the PET_ID code, extract out the ID from the URI,
+                    // so we know which row to update. Selection will be "_id=?" and selection
+                    // arguments will be a String array containing the actual ID.
+                    selection = PetContract.PetEntry._ID + "=?";
+                    selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                    return updatePet(uri, contentValues, selection, selectionArgs);
+                default:
+                    throw new IllegalArgumentException("Update is not supported for " + uri);
+            }
+        }
+
+    /**
+     * Update pets in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more pets).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updatePet(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // If the {@link PetEntry#COLUMN_PET_NAME} key is present,
+        // check that the name value is not null.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_NAME)) {
+            String name = values.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("Pet requires a name");
+            }
+        }
+
+        // If the {@link PetEntry#COLUMN_PET_GENDER} key is present,
+        // check that the gender value is valid.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_GENDER)) {
+            Integer gender = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_GENDER);
+            if (gender == null || !PetContract.PetEntry.isValidGender(gender)) {
+                throw new IllegalArgumentException("Pet requires valid gender");
+            }
+        }
+
+        // If the {@link PetEntry#COLUMN_PET_WEIGHT} key is present,
+        // check that the weight value is valid.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_WEIGHT)) {
+            // Check that the weight is greater than or equal to 0 kg
+            Integer weight = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+            if (weight != null && weight < 0) {
+                throw new IllegalArgumentException("Pet requires valid weight");
+            }
+        }
+
+        // No need to check the breed, any value is valid (including null).
+
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
             return 0;
         }
 
-        /**
+        // Otherwise, get writeable database to update the data
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Returns the number of database rows affected by the update statement
+        return database.update(PetContract.PetEntry.TABLE_NAME, values, selection, selectionArgs);
+    }
+
+
+    /**
          * Delete the data at the given selection and selection arguments.
          */
         @Override
-        public int delete (Uri uri, String selection, String[]selectionArgs){
+        public int delete (@NonNull Uri uri, String selection, String[]selectionArgs){
             return 0;
         }
 
@@ -179,7 +241,7 @@ public class PetProvider extends ContentProvider {
          * Returns the MIME type of data for the content URI.
          */
         @Override
-        public String getType (Uri uri){
+        public String getType (@NonNull Uri uri){
             return null;
         }
     }
