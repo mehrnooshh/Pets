d9f60450ba454f0bb273f027ca52835fe5f58f35
Mehrnoosh Hassani <hassani.mehrnoosh@gmail.com>
2018-02-22 12:59:23 -0800

    Implemented getType() method in ContentProvider

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -38,7 +39,6 @@ public final class PetContract {
      * as the ContentProvider hasn't been given any information on what to do with "staff".
      */
     public static final String PATH_PETS = "pets";
-
     /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
@@ -47,9 +47,21 @@ public final class PetContract {
 
         /** The content URI to access the pet data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
 
-        /* Define some constant values related to database regarding
-        to access and modify the tables
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+
+        /* Define some constant values related to pets' table of shelter database regarding
+        to access and modify that
          */
         public static final String TABLE_NAME = "pets";
         public static final String _ID = "_id";
--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -221,27 +221,48 @@ public class PetProvider extends ContentProvider {
             return 0;
         }
 
-        // Otherwise, get writeable database to update the data
+        // Otherwise, get writable database to update the data
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
         // Returns the number of database rows affected by the update statement
         return database.update(PetContract.PetEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 
-
     /**
          * Delete the data at the given selection and selection arguments.
          */
-        @Override
-        public int delete (@NonNull Uri uri, String selection, String[]selectionArgs){
-            return 0;
-        }
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+            case PET_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+    }
         /**
          * Returns the MIME type of data for the content URI.
          */
         @Override
-        public String getType (@NonNull Uri uri){
-            return null;
+        public String getType(Uri uri) {
+            final int match = sUriMatcher.match(uri);
+            switch (match) {
+                case PETS:
+                    return PetContract.PetEntry.CONTENT_LIST_TYPE;
+                case PET_ID:
+                    return PetContract.PetEntry.CONTENT_ITEM_TYPE;
+                default:
+                    throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+            }
         }
     }
