4746202a772a15747f89032127422636cf5a5f92
Mehrnoosh Hassani <hassani.mehrnoosh@gmail.com>
2018-03-08 19:18:55 -0800

    Updated EditorActivity to hide Delete option in the menu when the user is in “insert mode”

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -109,6 +109,9 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         if (mCurrentPetUri == null) {
             //This is a new pet, so change the app bar to say "Add a Pet"
             setTitle(getString(R.string.editor_activity_title_new_pet));
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete a pet that hasn't been created yet.)
+            invalidateOptionsMenu();
         } else {
             //Otherwise this is an existing one, so the app bar to say "Edit Pet"
             setTitle(getString(R.string.editor_activity_title_edit_pet));
@@ -256,6 +259,17 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
                 Toast.makeText(this, R.string.editor_update_pet_successful, Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new pet, hide the "Delete" menu item.
+        if (mCurrentPetUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
     }
 
     @Override
